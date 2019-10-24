552984ef3b23a91211871f7c98b7a97ad66120bc
kamiya-s-max <s.kamiya.f15@gmail.com>
2019-10-24 17:29:07 +0900

    indexページからsign_up,sign_out,sign_in機能を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :avatar])
+  end
+
+  def after_sign_out_path_for(resource)
+    '/users/sign_in'
   end
 end
