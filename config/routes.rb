aa79e51a40bde161b77849bed8c0afac0277b579
kamiya-s-max <s.kamiya.f15@gmail.com>
2019-12-01 17:53:39 +0900

    expertsのルーティングを各カテゴリindex用と新規expert登録用に分割

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'experts#index'
+  root 'categories#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:edit, :update, :show]
-  resources :experts
+  resources :experts, only: [:new, :create]
+  resources :categories, only: :index do
+    resources :experts, except: [:new, :create]
+  end
 end
