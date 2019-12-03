c5f3fe3ec3be221955825ef04b0faa2ff9858a22
kamiya-s-max <s.kamiya.f15@gmail.com>
2019-12-03 18:45:59 +0900

    user update時のフラッシュメッセージを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def update
     @user = current_user
     if @user.update(user_params)
-      redirect_to root_path
+      redirect_to root_path, notice: "user update successfully."
     else
       render :edit
     end
