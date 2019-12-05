6d7b04e9d20fe23462027d7caff738ce099e4dfe
kamiya-s-max <s.kamiya.f15@gmail.com>
2019-12-05 18:11:34 +0900

    本番環境用のデータベース設定とseedファイルを用意

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.create do |c|
+  c.id = 1
+  c.name = 'ビジネス'
+end
+
+Category.create do |c|
+  c.id = 2
+  c.name = 'スポーツ'
+end
+
+Category.create do |c|
+  c.id = 3
+  c.name = 'アウトドア趣味(スポーツ以外)'
+end
+
+Category.create do |c|
+  c.id = 4
+  c.name = 'ゲーム'
+end
+
+Category.create do |c|
+  c.id = 5
+  c.name = 'インドア趣味(ゲーム以外)'
+end
+
+def reset_id(categories)
+  connection = ActiveRecord::Base.connection()
+  connection.execute("select setval('#{tablename}_id_seq',(select max(id) from #{tablename}))")
+end
